bbc90f096e38caafdc2374e8c10bfb524352ce20
Olaf Archer <olaf@olaf.com>
2023-01-22 19:09:47 +0200

    connected

--- config/connectionDB.js
+++ config/connectionDB.js
@@ -1,15 +1,17 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.set("strictQuery", true);
 
-mongoose.set("strictQuery", false);
+const DB_PASS = process.env.DB_PASS;
+const DB_USER = process.env.DB_USER;
 
-const connectionDB = () => {
-  try {
-    const connection = mongoose.connect("mongodb://localhost:27017/bookStore");
-    console.log("Database connection was successful");
-  } catch (error) {
-    console.log(error, "have got an error");
-  }
-};
+const connectionDB = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.koy3o8h.mongodb.net/book-store?retryWrites=true&w=majority`;
 
-module.exports = connectionDB;
+mongoose
+  .connect(connectionDB, { useNewUrlparser: true })
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log(err));
 
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
--- index.js
+++ index.js
@@ -3,20 +3,12 @@ const connectionDB = require("./config/connectionDB");
 const app = express();
 const dotenv = require("dotenv").config();
 
-
 const PORT = process.env.PORT || 3088;
 
-connectionDB();
-
-app.use("/", (req,res) => {
-  res.send("Hello From Server Side")
-})
+app.use("/", (req, res) => {
+  res.send("Hello From Server Side");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server Is Running On ${PORT}`)
-}) 
-
-
-
-
-
+  console.log(`Server Is Running On ${PORT}`);
+});

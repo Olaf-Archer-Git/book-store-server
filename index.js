4d170e1ec7ea490417a082c42de92457765bbee8
Olaf Archer <olaf@olaf.com>
2023-01-22 13:02:42 +0200

    express

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
 
+const PORT = process.env.PORT || 3088
 
+app.listen(PORT, () => {
+  console.log(`Server Is Running On ${PORT}`)
+})

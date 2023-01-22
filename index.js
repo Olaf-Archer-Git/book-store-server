eb719ad3b3de90ee7d003f0fc5152ecc274599c6
Olaf Archer <olaf@olaf.com>
2023-01-22 21:56:02 +0200

    mongo

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const connectionDB = require("./config/connectionDB");
 const app = express();
 const dotenv = require("dotenv").config();
 
 const PORT = process.env.PORT || 3088;
+const authRouter = require("./routes/authRoute");
 
-app.use("/", (req, res) => {
-  res.send("Hello From Server Side");
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/api/user", authRouter);
 app.listen(PORT, () => {
   console.log(`Server Is Running On ${PORT}`);
 });

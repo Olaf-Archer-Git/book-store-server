ceea306f8afdcfe659cfdfd94f770cdf5045aac1
olto <olto@gmail.com>
2023-01-25 23:39:18 +0200

    auth

--- models/userModel.js
+++ models/userModel.js
@@ -2,43 +2,51 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 // Declare the Schema of the Mongo model
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: [{ type: ObjectId, ref: "Address" }],
+    favorite: [{ type: ObjectId, ref: "Product" }],
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: "user"
-  }
-});
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
   const salt = bcrypt.genSaltSync(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-
-userSchema.methods.isPasswordMatched = async function(enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password)
-}
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);

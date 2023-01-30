e0285a3e97348b12dff3e440bea99f030a72f6f3
olto <olto@gmail.com>
2023-01-30 20:57:55 +0200

    productCtrl

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -2,9 +2,12 @@ const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
 
 const createProduct = asyncHandler(async (req, res) => {
-  res.json({
-    message: "this is product route",
-  });
+  try {
+    const newProduct = await Product.create(req.body);
+    res.json(newProduct);
+  } catch (error) {
+    throw new Error(error, "create product, productCtrl");
+  }
 });
 
 module.exports = { createProduct };

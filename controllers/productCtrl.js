c4360fea88e7004aea8f25692e0ae397e91cbf2b
olto <olto@gmail.com>
2023-03-19 20:01:05 +0200

    poductCtrl

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -17,56 +17,29 @@ const createProduct = asyncHandler(async (req, res) => {
   }
 });
 
-//get all products
-const getAllProducts = asyncHandler(async (req, res) => {
+// update product
+const productUpdate = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoose(id);
   try {
-    //filter products
-    const queryObj = { ...req.query };
-    const excludefields = ["sort", "page", "limit", "fields"];
-    excludefields.forEach((item) => delete queryObj[item]);
-
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`
-    );
-
-    //sorting products
-    let query = Product.find(JSON.parse(queryString));
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
-
-    //limit the fields
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    }
-    // else {
-    //   query = query.select("__v");
-    // }
-    /////////////////////////
-
-    //pagination
-    const page = req.query.page;
-    const limit = req.query.limit;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const productCount = await Product.countDocuments();
-      if (skip >= productCount)
-        throw new Error("This Page Does Not Exist, pagination, productcount");
-    }
-
-    const product = await query;
-    res.json(product);
+    const updateProduct = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updateProduct);
   } catch (error) {
-    throw new Error(error, "getAllProducts, productCtrl");
+    throw new Error(error, "updateBlog error");
   }
 });
+
+//get all products
+const getAllProducts = async (req, res) => {
+  try {
+    const getProducts = await Product.find();
+    res.json(getProducts);
+  } catch (error) {
+    throw new Error(error, "getAllProductss error");
+  }
+};
 
 // get single product
 const getProduct = asyncHandler(async (req, res) => {
@@ -76,22 +49,6 @@ const getProduct = asyncHandler(async (req, res) => {
     res.json(findProduct);
   } catch (error) {
     throw new Error(error, "getProduct, productCtrl");
-  }
-});
-
-//update product
-const productUpdate = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  try {
-    if (req.body.title) {
-      req.body.slug = slugify(req.body.title);
-    }
-    const updateProduct = await Product.findOneAndUpdate(id, req.body, {
-      new: true,
-    });
-    res.json(updateProduct);
-  } catch (error) {
-    throw new Error(error, "productUpdate, productCtrl");
   }
 });
 
